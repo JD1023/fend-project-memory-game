0148a7c13622f0963c956fc4764d6f3dcae764bd
JD1023 <juandrevdw1@gmail.com>
2018-03-31 17:10:41 +0300

    feat: Create Deck
    Shuffle an array of cards and display cards on the user interface.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,55 @@
-/*
- * Create a list that holds all of your cards
- */
+// Array of cards
+let cards = [
+  'fa fa-cube',
+  'fa fa-cube',
+  'fa fa-paper-plane-o',
+  'fa fa-paper-plane-o',
+  'fa fa-bicycle',
+  'fa fa-bicycle',
+  'fa fa-bomb',
+  'fa fa-bomb',
+  'fa fa-leaf',
+  'fa fa-leaf',
+  'fa fa-diamond',
+  'fa fa-diamond',
+  'fa fa-anchor',
+  'fa fa-anchor',
+  'fa fa-bolt',
+  'fa fa-bolt'
+];
 
+//declare global variables
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+let firstCard;
+let secondCard;
+let moveCounter = 0;
+let matchCounter = 0;
+let timer;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let starCount = 5;
+const deck = document.querySelector(".deck");
+
+//Generate HTML for deck
+function createDeck() {
+    let shuffledCards = shuffle(cards);
+    deck.innerHTML = "";
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < shuffledCards.length; i++) {
+        let card = document.createElement("li");
+        let cardFace = document.createElement("i");
+        cardFace.className = shuffledCards[i];
+        card.classList.add("card");
+        card.appendChild(cardFace);
+        deck.appendChild(card);
+
+    }
+    deck.appendChild(fragment);
+
+}
+
+createDeck();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,14 +66,4 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+createDeck();
