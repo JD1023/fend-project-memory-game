32c85c48cedfeeeed99ca1e7de3ede11656bfbd5
JD1023 <juandrevdw1@gmail.com>
2018-03-31 18:20:28 +0300

    feat: Check if game is over
    Timer starts when game runs, and stops when games is over. Game over
    condition is met when there are 8 matches

--- js/app.js
+++ js/app.js
@@ -30,6 +30,7 @@ let minutes = 0;
 let hours = 0;
 let starCount = 5;
 const deck = document.querySelector(".deck");
+const displayTime = document.getElementById("time");
 
 //Generate HTML for deck
 function createDeck() {
@@ -47,7 +48,7 @@ function createDeck() {
 
     }
     deck.appendChild(fragment);
-
+    startTimer();
 }
 
 //Flip card when clicked and assign cards to variables
@@ -93,6 +94,30 @@ function checkMatch() {
     }
 }
 
+//Start timer
+
+function startTimer() {
+    timer = setInterval(function () {
+        seconds++;
+        if (seconds >= 60) {
+            minutes++;
+            seconds = 0;
+            if (minutes >= 60) {
+                hours++;
+                minutes = 0;
+            }
+        }
+        displayTime.textContent = "Time " + (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    }, 1000)
+}
+
+//check if game is over and stop timer
+
+function checkWin() {
+    if (matchCounter === 8) {
+      clearInterval(timer);
+    }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
